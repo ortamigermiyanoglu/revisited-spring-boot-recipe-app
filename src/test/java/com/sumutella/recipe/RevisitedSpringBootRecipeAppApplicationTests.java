400f68b93f3d48d7a74514d0e3e261d3fd6d16bd
Semir Umut Kurt <fatih.ek96@hotmail.com>
2019-12-23 10:06:48 +0300

    Initial commit

---
+++ src/test/java/com/sumutella/recipe/RevisitedSpringBootRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumutella.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisitedSpringBootRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
