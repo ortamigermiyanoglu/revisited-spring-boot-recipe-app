38033eb4837cd83eee0059ffa6a8574a72adfbf4
Semir Umut Kurt <fatih.ek96@hotmail.com>
2019-12-23 18:49:23 +0300

    index controller has been implemented

---
+++ src/main/java/com/sumutella/recipe/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package com.sumutella.recipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author sumutella
+ * @time 10:08 AM
+ * @since 12/23/2019, Mon
+ */
+@Controller
+public class IndexController {
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+        return "index";
+    }
+}
