47bc557d86871aaa4c83dbd3c179dae00368f626
Semir Umut Kurt <fatih.ek96@hotmail.com>
2019-12-25 13:26:51 +0300

    data jpa test

--- src/test/java/com/sumutella/recipe/repository/UnitOfMeasureRepositoryIT.java
+++ src/test/java/com/sumutella/recipe/repository/UnitOfMeasureRepositoryIT.java
@@ -1,6 +1,13 @@
 package com.sumutella.recipe.repository;
 
+import com.sumutella.recipe.model.UnitOfMeasure;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,9 +16,16 @@ import static org.junit.jupiter.api.Assertions.*;
  * @time 12:49 PM
  * @since 12/25/2019, Wed
  */
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
 class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
 
     @Test
     void findByDescription() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional =unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", unitOfMeasureOptional.get().getDescription());
     }
 }
